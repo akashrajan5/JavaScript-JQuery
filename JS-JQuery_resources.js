3964b7805a0ee754fe33bf141cebe7d92791cf22
Akash <83765015+akashrajan5@users.noreply.github.com>
2021-12-16 11:53:28 +0530

    Create JS-JQuery_resources.js

---
+++ JS-JQuery_resources.js
@@ -0,0 +1,11 @@
+//Selectors
+document.getElementById("id-name");
+document.getElementsByClassName("classname");
+document.querySelector(".classname || #id-name || element.classname || element['attribute'] || element,element");
+document.querySelectorAll(".classname || #id-name || element.classname || element['attribute'] || element,element"); //Select all the elements with selector
+$(".classname || #id-name || element.classname || element['attribute'] || element,element")
+
+
+//its better to use .value or .val() to get or set input element values.
+//use attr() to change data attribute values or use .data() if possible.
+//
