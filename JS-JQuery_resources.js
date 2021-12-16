ec379b4633aee899e85a07cd17e4bd0cb082ee0e
Akash <83765015+akashrajan5@users.noreply.github.com>
2021-12-16 11:57:24 +0530

    Update JS-JQuery_resources.js

--- JS-JQuery_resources.js
+++ JS-JQuery_resources.js
@@ -5,7 +5,9 @@ document.querySelector(".classname || #id-name || element.classname || element['
 document.querySelectorAll(".classname || #id-name || element.classname || element['attribute'] || element,element"); //Select all the elements with selector
 $(".classname || #id-name || element.classname || element['attribute'] || element,element")
 
+//Inner html
+//for javascript use .innerHTML
+//for jQuery use .html(), .text()
 
 //its better to use .value or .val() to get or set input element values.
 //use attr() to change data attribute values or use .data() if possible.
-//
