4db7943977a9be3ba013dbec686a58b997a148cd
Akash <83765015+akashrajan5@users.noreply.github.com>
2021-12-30 19:26:04 +0530

    Create Ajax_call.js

---
+++ Ajax_call.js
@@ -0,0 +1,10 @@
+    //Ajax post request + (GET)
+    $.ajax({
+        type:"post",
+        url:"",
+        data: {},
+        cache: false,
+        success: function(data) {
+           console.log(data);
+        }
+    });
